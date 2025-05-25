c53d311fe262f796c819c3f609ed2e395e2df0de
Meysam Zarei <mey.zarei@yahoo.com>
2025-05-25 17:08:29 +0200

    Implement invariant for merchant of orders

--- src/main/kotlin/io/billie/orders/domain/OrderFactory.kt
+++ src/main/kotlin/io/billie/orders/domain/OrderFactory.kt
@@ -1,5 +1,6 @@
 package io.billie.orders.domain
 
+import io.billie.orders.domain.exception.MerchantNotFoundException
 import io.billie.orders.domain.model.BuyerInfo
 import io.billie.orders.domain.model.MerchantInfo
 import io.billie.orders.domain.model.Money
@@ -13,14 +14,22 @@ import org.springframework.stereotype.Service
 internal class OrderFactory(private val merchantProviderAdapter: MerchantProviderAdapter) {
 
     fun createFromDto(orderDatabaseDto: OrderDatabaseDto): Order {
+
+        val merchantInfo = merchantProviderAdapter.findById(orderDatabaseDto.merchantId)
+        if (merchantInfo == null) {
+            throw MerchantNotFoundException("Merchant not found")
+        }
+
         return Order.create(
             orderDatabaseDto.id,
             Money.create(orderDatabaseDto.totalAmount),
             orderDatabaseDto.shipments.map {
                 shipment -> Shipment.create(shipment.key, shipment.value)
             }.toMutableList(),
-            // TODO:: Should be replaced with real one
-            MerchantInfo.create(orderDatabaseDto.merchantId, "test"),
+            MerchantInfo.create(
+                merchantInfo.id, merchantInfo.name
+            ),
+            // TODO:: Should be replaced with real one when the buyer context implemented
             BuyerInfo.create(orderDatabaseDto.buyerId, "test")
         )
     }
--- src/main/kotlin/io/billie/orders/infrastructure/MerchantProviderAdapter.kt
+++ src/main/kotlin/io/billie/orders/infrastructure/MerchantProviderAdapter.kt
@@ -2,10 +2,13 @@ package io.billie.orders.infrastructure
 
 import io.billie.merchants.domain.MerchantProviderInterface
 import io.billie.merchants.domain.dto.MerchantExposeDto
+import io.billie.merchants.infrastructure.MerchantQueryService
 import java.util.UUID
 
-open class MerchantProviderAdapter(): MerchantProviderInterface {
+open class  MerchantProviderAdapter(
+    val merchantQueryService: MerchantQueryService
+): MerchantProviderInterface {
     override fun findById(id: UUID): MerchantExposeDto? {
-        TODO("Not yet implemented")
+        return merchantQueryService.findById(id)
     }
 }
